08f2ab432e868dc7558987053599c1510c94bab5
Iryna Semenenko <iryna.semenenko90@gmail.com>
2022-07-26 11:24:37 +0300

    Add task2 calculator

---
+++ Simple Calculator/src/Calculator.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number");
+
+        if (sc.hasNextDouble()) {
+            double firstValue = sc.nextDouble();
+            System.out.println("Input is correct: " + firstValue);
+
+            System.out.println("Enter an operation (for example: +, -, *, /, %)");
+            String operation = sc.next();
+            System.out.println(operation);
+            if (operation.equals("+") |
+                    operation.equals("-") |
+                    operation.equals("*") |
+                    operation.equals("/") |
+                    operation.equals("%") ) {
+                System.out.println("User inputs supported operations");
+
+                System.out.println("Enter the second number");
+                if (sc.hasNextDouble()) {
+                    double secondValue = sc.nextDouble();
+                    System.out.println("Input is correct: " + secondValue);
+                } else {
+                    System.out.println("Input is wrong (should be a number)");
+                }
+            } else {
+                System.out.println("User inputs unsupported operations");
+            }
+        } else {
+            System.out.println("Input is wrong (should be a number)");
+        }
+    }
+}
